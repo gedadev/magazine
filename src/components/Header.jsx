861198109685e7520627516c344c04d0b46c57cf
Gedaias Jair <gedaias@geda.dev>
2024-04-09 22:30:14 -0600

    Create menu activation transition

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,34 @@
+import { useRef, useState } from "react";
 import "../styles/header.css";
 
 export default function Header() {
+  const menu = useRef(null);
+  const [activeMenu, setActiveMenu] = useState(false);
+
+  const toggleMenu = () => {
+    if (!activeMenu) {
+      menu.current.style.transform = "scale(1)";
+    } else {
+      menu.current.style.transform = "scale(0)";
+    }
+    setActiveMenu(!activeMenu);
+  };
+
+  const refElement = (element) => (menu.current = element);
+
   return (
     <header>
       <h1>Vibes</h1>
       <nav>
-        <div className="ham-menu">
+        <div
+          className={`ham-menu ${activeMenu ? "active" : ""}`}
+          onClick={toggleMenu}
+        >
           <span className="bar"></span>
           <span className="bar"></span>
           <span className="bar"></span>
         </div>
-        <div className="menu-list">
+        <div className="menu-list" ref={refElement}>
           <ul>
             <li>Home</li>
             <li>Models</li>
