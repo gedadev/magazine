413709e8eaba3a615a64240ab5b991094564be8a
Gedaias Jair <gedaias@geda.dev>
2024-04-10 21:08:15 -0600

    Create Cover component

---
+++ src/components/Cover.jsx
@@ -0,0 +1,11 @@
+import "../styles/cover.css";
+
+export default function Cover() {
+  return (
+    <div className="cover">
+      <img src="https://i.imgur.com/oo26Sf4.jpg" alt="model" />
+      <span className="cover-name">FASHION</span>
+      <h2 className="cover-title">The new muse of the catwalk</h2>
+    </div>
+  );
+}
