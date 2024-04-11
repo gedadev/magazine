3e347f052e78cc2f31833c1e36fd1d9154ef7307
Gedaias Jair <gedaias@geda.dev>
2024-04-10 20:29:03 -0600

    Update section className and add a Cover component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./styles/App.css";
 import Header from "./components/Header";
+import Cover from "./components/Cover";
 
 function App() {
   return (
     <>
-      <section className="cover">
+      <section className="cover-container">
         <Header />
+        <Cover />
       </section>
       <main></main>
     </>
