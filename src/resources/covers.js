7f997d5501eb1b5cc378bd2862df8f4a0e3e795e
Gedaias Jair <gedaias@geda.dev>
2024-04-11 19:36:00 -0600

    Create file with the information of each cover

---
+++ src/resources/covers.js
@@ -0,0 +1,20 @@
+import { v4 as uuidv4 } from 'uuid';
+
+const covers = [
+    {
+      id: uuidv4(),
+      img: "https://i.imgur.com/oo26Sf4.jpg",
+      alt: "model",
+      name: "MODEL",
+      title: "The new muse of the catwalk",
+    },
+    {
+      id: uuidv4(),
+      img: "https://i.imgur.com/moBC8kp.jpg",
+      alt: "model",
+      name: "FASHION",
+      title: "Fresh summer fashion",
+    },
+];
+
+export default covers;
