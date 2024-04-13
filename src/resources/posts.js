ac632c763537424e122763bbbc620044126c9ab4
Gedaias Jair <gedaias@geda.dev>
2024-04-12 22:57:02 -0600

    Create file to store the posts information

---
+++ src/resources/posts.js
@@ -0,0 +1,78 @@
+import { v4 as uuidv4 } from 'uuid';
+
+const posts = [
+    {
+      id: uuidv4(),
+      date: 1712978391000,
+      img: "https://images.unsplash.com/photo-1712981412233-ef4f23b79c1a",
+      alt: "post image",
+      title: "The new muse of the catwalk",
+      content:
+        "Ipsum pariatur nostrud non officia adipisicing laborum excepteur ipsum qui deserunt elit veniam.",
+    },
+    {
+      id: uuidv4(),
+      date: 1712978391000,
+      img: "https://images.unsplash.com/photo-1712981412213-7343f7c23636",
+      alt: "post image",
+      title: "Fresh summer fashion",
+      content:
+        "Ipsum pariatur nostrud non officia adipisicing laborum excepteur ipsum qui deserunt elit veniam.",
+    },
+    {
+      id: uuidv4(),
+      date: 1712978391000,
+      img: "https://images.unsplash.com/photo-1712981412224-9ffc0c8be3a5",
+      alt: "post image",
+      title: "The truth about space",
+      content:
+        "Ipsum pariatur nostrud non officia adipisicing laborum excepteur ipsum qui deserunt elit veniam.",
+    },
+    {
+      id: uuidv4(),
+      date: 1712978391000,
+      img: "https://images.unsplash.com/photo-1712981412226-b06d7730f6c3",
+      alt: "post image",
+      title: "Fresh summer fashion",
+      content:
+      "Ipsum pariatur nostrud non officia adipisicing laborum excepteur ipsum qui deserunt elit veniam.",
+    },
+    {
+      id: uuidv4(),
+      date: 1712978391000,
+      img: "https://images.unsplash.com/photo-1712981412236-3df85819c968",
+      alt: "post image",
+      title: "The truth about space",
+      content:
+        "Ipsum pariatur nostrud non officia adipisicing laborum excepteur ipsum qui deserunt elit veniam.",
+    },
+    {
+      id: uuidv4(),
+      date: 1712978391000,
+      img: "https://images.unsplash.com/photo-1712981412213-8f6bf5003b45",
+      alt: "post image",
+      title: "The new muse of the catwalk",
+      content:
+        "Ipsum pariatur nostrud non officia adipisicing laborum excepteur ipsum qui deserunt elit veniam.",
+    },
+    {
+      id: uuidv4(),
+      date: 1712978391000,
+      img: "https://images.unsplash.com/photo-1712981412216-54ce3e6ab277",
+      alt: "post image",
+      title: "The truth about space",
+      content:
+        "Ipsum pariatur nostrud non officia adipisicing laborum excepteur ipsum qui deserunt elit veniam.",
+    },
+    {
+      id: uuidv4(),
+      date: 1712978391000,
+      img: "https://images.unsplash.com/photo-1712981412215-fcef34050813",
+      alt: "post image",
+      title: "Fresh summer fashion",
+      content:
+        "Ipsum pariatur nostrud non officia adipisicing laborum excepteur ipsum qui deserunt elit veniam.",
+    },
+  ];
+
+export default posts;
