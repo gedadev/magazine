e50c3a8df0fa8e13feeab14106cbc7c233d0c98c
Gedaias Jair <gedaias@geda.dev>
2024-04-14 22:26:49 -0600

    Add styles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,14 +4,36 @@ import posts from "../resources/posts";
 export default function Home() {
   return (
     <section className="home-section">
-      {posts.map((post) => (
-        <div key={post.id} className="post-item">
-          <img src={post.img} alt={post.alt} />
-          <span>{post.date}</span>
-          <h3>{post.title}</h3>
-          <p>{post.content}</p>
-        </div>
-      ))}
+      <div className="post-column">
+        {posts.map((post, index) => {
+          if (index % 2 === 0)
+            return (
+              <div key={post.id} className="post-item">
+                <img src={post.img} alt={post.alt} />
+                <span className="post-date">
+                  {new Date(post.date).toDateString()}
+                </span>
+                <h3>{post.title}</h3>
+                <p>{post.content}</p>
+              </div>
+            );
+        })}
+      </div>
+      <div className="post-column">
+        {posts.map((post, index) => {
+          if (index % 2 !== 0)
+            return (
+              <div key={post.id} className="post-item">
+                <img src={post.img} alt={post.alt} />
+                <span className="post-date">
+                  {new Date(post.date).toDateString()}
+                </span>
+                <h3>{post.title}</h3>
+                <p>{post.content}</p>
+              </div>
+            );
+        })}
+      </div>
     </section>
   );
 }
